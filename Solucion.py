ee65f80b5aeab95fdaabf5f14541ce8cbbad8767
Santiago Cordoba <scordoba067@soyudemedellin.edu.co>
2024-02-14 10:58:25 -0500

    Initial commit

---
+++ Solucion.py
@@ -0,0 +1,11 @@
+# Punto N 1
+print("por favor ingrese su nombre")
+nombre = input()
+print("por favor ingrese su edad")
+edad = input()
+print("usted se llama " + nombre + " y tiene " + edad + " años de edad")
+
+# Punto N 2
+def Calcular_radio(radio, area):
+    area = radio * radio * 3,14
+    radio= 20
